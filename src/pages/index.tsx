59b7ec0eabcfb2a20cefad6d971f9b88472ec127
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-09-16 23:47:38 +0900

    [追加]フッターコンポーネント

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import { css } from '@emotion/react'
 
 import { breakpoints, fonts } from '@/styles/settings'
 import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer'
 
 const HomePage: NextPage = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
