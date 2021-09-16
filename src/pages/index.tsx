0b4d822dce9e8879703eb8cb8ef6161cd4e08896
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-09-17 00:55:28 +0900

    [追加]SectionAboutコンポーネント作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,31 @@ import { css } from '@emotion/react'
 import { breakpoints, fonts } from '@/styles/settings'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
+import { SectionAbout } from '@/components/section/SectionAbout'
 
 const HomePage: NextPage = () => {
   return (
     <>
       <Header />
+      <main>
+        <article css={article}>
+          <SectionAbout />
+        </article>
+      </main>
       <Footer />
     </>
   )
 }
 
 export default HomePage
+
+const article = css`
+  display: flex;
+  max-width: 950px;
+  flex-direction: column;
+  padding: 0 25px;
+  padding-top: 200px;
+  padding-bottom: 120px;
+  margin: 0 auto;
+  gap: 240px;
+`
