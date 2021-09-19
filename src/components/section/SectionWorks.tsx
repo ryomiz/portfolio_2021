97e1b4b28b2f9efdeec833ff6617baee2d670c76
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-09-19 23:06:46 +0900

    [追加]SectionWorksコンポーネント作成
    getStaticPropsを使用したSSG

---
+++ src/components/section/SectionWorks.tsx
@@ -0,0 +1,27 @@
+import { css } from '@emotion/react'
+
+import { Section } from '@/components/Section'
+import { WorkItem } from '@/components/unique/works/WorkItem'
+
+type Props = {
+  data: Array<{ workTitle: string; src: string }>
+}
+
+export const SectionWorks: React.VFC<Props> = (props) => {
+  const { data } = props
+  return (
+    <Section title="Works">
+      <div css={grid}>
+        {data.map((item) => (
+          <WorkItem key={item.workTitle} data={item} />
+        ))}
+      </div>
+    </Section>
+  )
+}
+
+const grid = css`
+  display: grid;
+  gap: 60px;
+  grid-template-columns: repeat(2, 1fr);
+`
