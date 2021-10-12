02c587bc6aead9d27e588421394295773f30be01
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-10-12 12:14:54 +0900

    [修正]WorkItem スタイル修正

--- src/components/unique/index/parts/works/WorkItem.tsx
+++ src/components/unique/index/parts/works/WorkItem.tsx
@@ -13,18 +13,25 @@ type Props = {
 
 export const WorkItem: React.VFC<Props> = (props) => {
   const { data } = props
-  const { title, image } = data
   const { openModal } = useModal()
-  console.log(data)
 
   return (
     <>
       <div css={container} onClick={openModal}>
         <div css={description}>
-          <h3 css={workTitle}>{title}</h3>
+          <h3 css={workTitle}>{data.title}</h3>
           <button css={detail}>詳しく見る</button>
         </div>
-        <Image src={image.url} width={420} height={236} alt={title} />
+        <div css={image}>
+          <div css={imageInner}>
+            <Image
+              src={data.image.url}
+              width={800}
+              height={800}
+              alt={data.title}
+            />
+          </div>
+        </div>
       </div>
       <WorkDetailModal data={data} />
     </>
@@ -61,4 +68,19 @@ const workTitle = css`
 const detail = css`
   font-size: 1.3rem;
   font-weight: 700;
+`
+
+const image = css`
+  position: relative;
+  overflow: hidden;
+  order: 1;
+  padding-top: 60%;
+`
+
+const imageInner = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 `
